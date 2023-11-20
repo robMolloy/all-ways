041293eb407aa9444f4dc30509df3be122808b9c
robmolloyntt <robert.molloy@nttdata.com>
2023-11-20 11:15:04 +0000

    fetch-data-styles

---
+++ projects/add-fetched-data-to-list-on-button-click/js/script.js
@@ -0,0 +1,49 @@
+const apiUrlBase = "https://pokeapi.co/api/v2/pokemon/";
+const delay = (x) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(true), x);
+  });
+};
+
+const fetchPokemon = async (pokeId) => {
+  await delay(2000);
+  const url = `${apiUrlBase}${pokeId}`;
+  const resp = await fetch(url);
+  const pokemon = await resp.json();
+  return pokemon;
+};
+
+const getRandomPokeId = () => {
+  return Math.floor(Math.random() * 150) + 1;
+};
+
+const setLoadingState = (isLoading) => {
+  const buttonElement = document.getElementById("add-pokemon-button");
+  if (isLoading === true) buttonElement.classList.add("is-loading");
+  if (isLoading === false) buttonElement.classList.remove("is-loading");
+};
+
+const getLoadingState = () => {
+  const buttonElement = document.getElementById("add-pokemon-button");
+  return buttonElement.classList.contains("is-loading");
+};
+
+const fetchRandomPokemonAndAddToList = async () => {
+  const listElement = document.getElementById("pokemon-list");
+  const pokemon = await fetchPokemon(getRandomPokeId());
+  listElement.insertAdjacentHTML(
+    "beforeend",
+    `<li><a class="rounded-md">${pokemon.name}</a></li>`
+  );
+};
+
+const handleAddPokemonEvent = async () => {
+  const isLoading = getLoadingState();
+  if (isLoading) return;
+
+  setLoadingState(true);
+  await fetchRandomPokemonAndAddToList();
+  setLoadingState(false);
+};
+
+// handleAddPokemonEvent();
